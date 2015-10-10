8a6289929fa861da5f0971d8913c03de7947eca4
Manuel Leduc <manuel.leduc@inria.fr>
2015-10-10 02:47:42 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kevoree_dotnet_comp_ticker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
